376be8f6611d82282455e58433f3cf93741e15b7
Jason Frey <fryguy9@gmail.com>
2025-05-20 21:42:52 -0400

    Initial commit

---
+++ lib/jsonata.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jsonata/version"
+
+module Jsonata
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jsonata_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Jsonata do
+  it "has a version number" do
+    expect(Jsonata::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
